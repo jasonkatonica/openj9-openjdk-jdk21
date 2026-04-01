057b68e1d83c6994502ff192f3b9674adb849e57
JinhangZhang <Jinhang.Zhang@ibm.com>
2026-04-01 00:45:44 -0400

    use generic instead

--- src/java.base/share/classes/sun/security/ssl/Hybrid.java
+++ src/java.base/share/classes/sun/security/ssl/Hybrid.java
@@ -384,7 +384,7 @@ public class Hybrid {
             implements SecretKey {
         @Override
         public String getAlgorithm() {
-            return "TlsPremasterSecret";
+            return "Generic";
         }
 
         @Override
