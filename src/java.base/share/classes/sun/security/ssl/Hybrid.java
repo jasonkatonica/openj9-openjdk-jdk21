574dfabd95e2e18c2a9ba0d52cb257234925c632
Jason Katonica <katonica@us.ibm.com>
2026-04-16 17:58:13 -0400

    Another fix for algorithm

--- src/java.base/share/classes/sun/security/ssl/Hybrid.java
+++ src/java.base/share/classes/sun/security/ssl/Hybrid.java
@@ -110,7 +110,8 @@ public class Hybrid {
         if (name.startsWith("secp") || name.equals("X25519")) {
             return KEM.getInstance("DH", HybridProvider.PROVIDER);
         } else {
-            return KEM.getInstance("ML-KEM");
+            // Pass the specific ML-KEM variant (e.g., "ML-KEM-768" or "ML-KEM-1024")
+            return KEM.getInstance(name);
         }
     }
 
