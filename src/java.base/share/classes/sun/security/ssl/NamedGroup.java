86bc5e8e4eefbfd50c1939b72d04b75064ed2e75
Jason Katonica <katonica@us.ibm.com>
2026-07-06 14:34:58 -0400

    Add X25519MLKEM768 to the top
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

--- src/java.base/share/classes/sun/security/ssl/NamedGroup.java
+++ src/java.base/share/classes/sun/security/ssl/NamedGroup.java
@@ -919,6 +919,9 @@ enum NamedGroup {
                 }
             } else {        // default groups
                 NamedGroup[] groups = new NamedGroup[] {
+                        // Hybrid key agreement
+                        X25519MLKEM768,
+
                         // Primary XDH (RFC 7748) curves
                         X25519,
 
@@ -937,8 +940,6 @@ enum NamedGroup {
                         FFDHE_6144,
                         FFDHE_8192,
 
-                        // Hybrid key agreement
-                        X25519MLKEM768,
                     };
 
                 groupList = new ArrayList<>(groups.length);
