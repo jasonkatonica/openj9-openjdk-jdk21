153081b2b84e3726eccbecdda0b4032b518cf101
Jason Katonica <katonica@us.ibm.com>
2026-07-07 14:53:49 -0400

    Revert "remove generic"
    This reverts commit db4f5dc8de5a04abceb99a6a0f245ea49aa8a60a.

--- src/java.base/share/classes/sun/security/ssl/KAKeyDerivation.java
+++ src/java.base/share/classes/sun/security/ssl/KAKeyDerivation.java
@@ -51,6 +51,9 @@ import jdk.internal.access.SharedSecrets;
  */
 public class KAKeyDerivation implements SSLKeyDerivation {
 
+    // Algorithm used to derive TLS 1.3 shared secrets
+    private static final String t13KeyDerivationAlgorithm =
+            System.getProperty("jdk.tls.t13KeyDerivationAlgorithm", "Generic");
     private final String algorithmName;
     private final HandshakeContext context;
     private final PrivateKey localPrivateKey;
@@ -252,7 +255,7 @@ public class KAKeyDerivation implements SSLKeyDerivation {
                     var decapsulator = kem.newDecapsulator(localPrivateKey);
                     sharedSecret = decapsulator.decapsulate(
                             keyshare, 0, decapsulator.secretSize(),
-                            "TlsPremasterSecret");
+                            t13KeyDerivationAlgorithm);
                 } catch (IllegalArgumentException | InvalidKeyException |
                         DecapsulateException e) {
                     // Peer validation failure
@@ -270,7 +273,7 @@ public class KAKeyDerivation implements SSLKeyDerivation {
                 KeyAgreement ka = KeyAgreement.getInstance(algorithmName);
                 ka.init(localPrivateKey);
                 ka.doPhase(peerPublicKey, true);
-                sharedSecret = ka.generateSecret("TlsPremasterSecret");
+                sharedSecret = ka.generateSecret(t13KeyDerivationAlgorithm);
             }
 
             return deriveHandshakeSecret(type, sharedSecret);
